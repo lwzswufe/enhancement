9c46a7e64f638c8c0b64f08913d5d5327c71220a
lwzswufe <lwzswufe@foxmail.com>
2016-10-23 11:12:38 +0800

    for r and matlab

--- file_encode_utf_8.py
+++ file_encode_utf_8.py
@@ -6,8 +6,9 @@ import sys
 filePathSrc = "D:\\Code\\Code\\"  # Path to the folder with files to convert
 for root, dirs, files in os.walk(filePathSrc):
     for fn in files:
-        if fn[-2:] == '.R':  # Specify type of the files
+        if (fn[-2:] == '.R') | (fn[-2:] == '.m') | (fn[-2:] == '.py'):  # Specify type of the files
             notepad.open(root + "\\" + fn)
             notepad.runMenuCommand("Encoding", "Convert to UTF-8")
             notepad.save()
             notepad.close()
+		
