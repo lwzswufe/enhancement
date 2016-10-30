bc6248d26daeeeaf265b35e431cf2061a4a32916
lwzswufe <lwzswufe@foxmail.com>
2016-10-30 20:38:54 +0800

    01

--- CART/ID3Dtree.py
+++ CART/ID3Dtree.py
@@ -13,12 +13,20 @@ class ID3_tree(object):
         self.dataset = []  # 数据集
         self.labels = []  # 标签集
 
-    def load_dataset(self, fname, labels):
+    def load_dataset(self, labels=['age', 'revenue', 'student', 'credit'],
+                     fname='D:\\Cache\\dataset.dat',):
         record_list = []
-        f = open(fname, 'rb')
+        f = open(fname, 'r')
         content = f.read()
         row_list = content.splitlines()  # 转化为一维表
         record_list = [row.split("\t") for row in row_list if row.strip()]
+        # for row in record_list:
+        #    if row[4] == "yes":
+        #        row[4] = 1
+        #    else:
+        #        row[4] = 0
+        #    for d in range(len(row)):
+        #        row[d] = int(row[d])
         self.dataset = record_list
         self.labels = labels
 
@@ -29,21 +37,21 @@ class ID3_tree(object):
     def build_tree(self, label, dataset):
         cate_list = [data[-1] for data in dataset]  # 抽取源数据集的决策标签列
         # 程序终止条件1：如果cate_list 只有一种决策标签，停止划分，返回这个决策标签
-        if cate_list.count((cate_list[0]) == len(cate_list)):  # count方法 统计出现次数
+        if cate_list.count(cate_list[0]) == len(cate_list):  # count方法 统计出现次数
             return cate_list[0]
         # 程序终止条件2：如果数据集的第一个决策标签只有一个，则返回这个决策标签
         if len(dataset[0]) == 1:
             return self.max_cate(cate_list)
         # 算法核心
-        best_feat = self.get_best_feat()
+        best_feat = self.get_best_feat(dataset)
         best_feat_label = label[best_feat]
         tree = {best_feat_label:{}}
         del label[best_feat]
         # 抽取最优特征值向量
-        unique_vals = set([data[best_feat] for data in self.dataset])
+        unique_vals = set([data[best_feat] for data in dataset])
         for value in unique_vals:
             sub_labels = label[:]
-            split_data = self.split_dataset(self.dataset, best_feat, value)
+            split_data = self.split_dataset(best_feat, value, dataset)
             sub_tree = self.build_tree(sub_labels, split_data)  # 递归
             tree[best_feat_label][value] = sub_tree
         return tree
@@ -55,7 +63,7 @@ class ID3_tree(object):
     def get_best_feat(self, dataset):
         # 计算特征向量维， 种种最后一列用于类别标签， 因此要减去
         num_features = len(dataset[0]) - 1
-        base_entropy = self.compute_entropy()
+        base_entropy = self.compute_entropy(dataset)
         best_info_gain = 0.0  # 最优信息增益
         best_futures = -1
         # 外循环遍历数据集各列，计算最优特征轴
@@ -80,7 +88,7 @@ class ID3_tree(object):
         data_len = float(len(dataset))
         cate_list = [data[-1] for data in dataset]  # 获取类别标签
         # 得到类别为key 出现次数为value的字典
-        items = dict([(cate_list.count(i), i) for i in cate_list])
+        items = dict([(i, cate_list.count(i)) for i in cate_list])
         info_entropy = 0.0
         for key in items:
             prob = float(items[key]) / data_len  # 香农熵 = -p * log2(p)
@@ -109,4 +117,7 @@ class ID3_tree(object):
         return class_label
 
 if __name__ == "__main__":
-    print('hello')
+    id3 = ID3_tree()
+    id3.load_dataset()
+    id3.train()
+    print(id3.tree)
