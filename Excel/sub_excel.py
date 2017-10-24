0e2b5af19fca02d7aa76b39bd6c165a5ceb98994
wnzqs <lwzswufe@163.com>
2017-10-24 23:42:17 +0800

    init python_excel

---
+++ Excel/sub_excel.py
@@ -0,0 +1,44 @@
+# author='lwz'
+# coding:utf-8
+# !/usr/bin/env python3
+import xlrd  # 打开模块
+import xlwt  # 写入模块
+
+
+def read_excel():
+    #    打开文件 读取模式
+    workbook = xlrd.open_workbook(r'D:\附件4 政府性债务投资项目资产明细（3张表）.xlsx')
+    #   打开指定sheet
+    sheet2 = workbook.sheet_by_name('存量债务')
+    
+    name_list = list()    # 表名列表
+    workbook_dict = {}   # 文件名字典{文件名：workbook对象}
+    pre_name = ""        # 前一个表名
+    row_flag = 0         # 写入文件行指针
+    for i in range(4, sheet2.nrows):
+        if pre_name != sheet2.cell(i, 3).value and sheet2.cell(i, 3).value not in name_list:
+            # 判断与前一个表名是否一致 表名是否重复
+            name_list.append(sheet2.cell(i, 3).value)
+            pre_name = sheet2.cell(i, 3).value   # 获取当前表名
+            fname = "D:\\" + pre_name + ".xlsx"  # 生成文件名
+            print(fname)
+            new_workbook = xlwt.Workbook(fname)  # 创建excel 得到workbook对象
+            workbook_dict[fname] = new_workbook  # 记录workbook对象
+            new_workbook.add_sheet("sheet1")     # 创建 sheet
+            new_sheet = new_workbook.get_sheet(0)  # 获取第一个sheet
+
+            new_sheet.write(0, 0, sheet2.cell(1, 0).value)  # 写入表格名称  对sheet(0, 0)写入sheet2.cell(1, 0).value
+            row_flag = 1                         # 初始化指针 即当前写入行是第一行
+            for j in range(19):                 # 写入每一列的名称
+                new_sheet.write(row_flag, j, sheet2.cell(3, j).value)
+
+        row_flag += 1                            # 转到下一列
+        for j in range(19):
+                new_sheet.write(row_flag, j, sheet2.cell(i, j).value)  # 写入每一列的数据
+
+    for fname in workbook_dict:
+        workbook_dict[fname].save(fname)         # 保存workbook对象
+
+ 
+if __name__ == '__main__':
+    read_excel()
