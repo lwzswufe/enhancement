c82fdae5156eb0385726fe5eba5ebf6761b1f49b
lwzswufe <lwzswufe@foxmail.com>
2018-08-14 22:59:21 +0800

    增加多线程join的说明

--- MultiProcess/multi_thread.py
+++ MultiProcess/multi_thread.py
@@ -16,7 +16,7 @@ def task_agent(thread_id=0):
         if len(MSG) > 0:
             for msg in MSG:
                 print("thread {} get {}".format(thread_id, msg))
-                if msg == "stop":
+                if msg == "stop":   # 退出循环 关闭子进程
                     print("thread {} end".format(thread_id))
                     return
         time.sleep(0.2)             # 等待主线程关闭任务
@@ -32,14 +32,20 @@ def task_main():
             event.clear()           # 重置event状态
             print(MSG)
     MSG.append("stop")              # 关闭子线程
-    event.set()
+    event.set()                     # 唤醒子线程 让其自行关闭
 
 
 if __name__ == "__main__":
     event = Event()
     th_0 = Thread(target=task_main, args=())
+    th_list = [th_0]
     for i in range(3):
         th = Thread(target=task_agent, args=(i, ))
+        th_list.append(th)
         th.start()
     th_0.start()
+    for th in th_list:
+        th.join()                   # 阻塞 等待子线程结束
+    
+
 
