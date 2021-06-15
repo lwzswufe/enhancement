a76ce99ad95782660718cd495d8fee8e3b61b932
lwzswufe <lwzswufe@foxmail.com>
2021-06-15 23:02:39 +0800

    修复C++编译Python包中类函数的bug

--- CPython/Custom_use.py
+++ CPython/Custom_use.py
@@ -3,15 +3,19 @@ import os
 sys.path.append(os.path.abspath("."))
 print(os.path.abspath("."))
 import custom
+import time
 
 '''
 运行前需要将 Custom.cpp 编译为 custom.pyd
 '''
 Donald_Trump = custom.Custom("Donald", "Trump", 1)          # __new__()  __init__()
-Hillary_Clinton = custom.Custom("Hillary", "Clinton", 1)    # __new__()  __init__()
+Hillary_Clinton = custom.Custom("Hillary", "Clinton")       # __new__()  __init__()
+
 del Hillary_Clinton                                         # __del__()
-full_name = Donald_Trump.name()                             # 使用类方法
+full_name = Donald_Trump.get_name()                         # 使用类方法
 print(full_name)
-Donald_Trump.change_name("Hillary")
+Donald_Trump.change_name("Hillary", "Trump")
+print("next")
+full_name = Donald_Trump.get_name()  
 print(full_name)
 print("program  end.....")
